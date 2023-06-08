f786d91d2c36628f0a267b710172e9a1c8c9fd27
ChristianHarrison <christianh@live.co.uk>
2023-06-08 15:50:11 +0100

    Refactor to include server and client dirs

---
+++ server/server_app.py
@@ -0,0 +1,20 @@
+import socket 
+import time
+sock = socket.socket()
+
+port = 12345
+
+sock.bind(('', port))
+
+sock.listen(1)
+print('listening for client')
+
+
+while True:
+    
+    client, address = sock.accept()
+    print(f"Got connection from: {address}")
+    
+    time.sleep(2)
+    while True:
+        client.send(input('Enter some data: ').encode())
