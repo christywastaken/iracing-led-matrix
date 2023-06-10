05408fa5e4567058ea7c5d5f5cd9f2244032f6d2
Christian <christianh@live.co.uk>
2023-06-10 13:49:44 +0100

    Adds error handling

--- client/client_app.py
+++ client/client_app.py
@@ -9,9 +9,11 @@ PC_IP = '10.83.1.178'
 
 sock.connect((PC_IP, PORT))
 
-while True:
-    data = sock.recv(1024)
-    process_data(data)
-    print(data)
-    
+try:
+    while True:
+        data = sock.recv(1024)
+        process_data(data)
+        print(data)
+except KeyboardInterrupt:
+    print('Client terminated.')
 
