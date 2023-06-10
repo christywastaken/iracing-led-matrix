bf6240865ed3528c5bbdfa26bef9e53c2672cd82
Christian <christianh@live.co.uk>
2023-06-10 11:56:16 +0100

    Small refactor

--- client/client_app.py
+++ client/client_app.py
@@ -9,9 +9,9 @@ PC_IP = '10.83.1.178'
 
 sock.connect((PC_IP, PORT))
 
-
 while True:
     data = sock.recv(1024)
     process_data(data)
+    print(data)
     
-    
+
--- client/matrix_model.py
+++ client/matrix_model.py
@@ -15,16 +15,19 @@ matrix = RGBMatrix(options=options)
 
 gears_coords_list = []
 
+
 with open('../client/gears_coords.txt', 'r') as f:
     for line in f:
         line.strip()
         data_list = ast.literal_eval(line)
         gears_coords_list.append(data_list)
 
+
 def display_gear(gear_coords: list):
     matrix.Clear()
     for x, y  in gear_coords:
         matrix.SetPixel(x, y, 255, 0, 0)
+
 
 def select_gear(gear: str):
     try:
@@ -33,6 +36,7 @@ def select_gear(gear: str):
     except IndexError as err:
         print(f'Error: {err}')
       
+
 def process_data(data):
      try:
         data_str = data.decode()
